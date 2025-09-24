4290f9575286025f9705144f140632b55d383efa
weerakoonpubuduni-dotcom <weerakoonpubuduni@gmail.com>
2025-09-24 20:56:36 +0530

    initial commit

---
+++ src/test/java/com/myfinance/ead_my_finance_backend/EadMyFinanceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myfinance.ead_my_finance_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EadMyFinanceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
